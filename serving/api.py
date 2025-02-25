eda067e7ad9f7d292f727c02addb8eed4c12dfba
Matthieu <randriantsoamatthieu@gmail.com>
2025-02-25 16:46:10 +0100

    Transform resquest data into dataframe

--- serving/api.py
+++ serving/api.py
@@ -1,10 +1,7 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
 import joblib
 import pandas as pd
+from fastapi import FastAPI
 
-class Request(BaseModel):
-    data: dict
 
 def drop_columns(X):
     columns = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"]
@@ -20,22 +17,28 @@ async def root():
     return {"message": "Hello World"}
 
 @app.post("/predict/")
-async def perdict(request: Request):
+async def perdict(passenger: dict):
 
     try:
-        if not isinstance(request.data, dict):
+        if not isinstance(passenger, dict):
             raise ValueError("Invalid input data")
 
         # List of required columns (update this based on your model's requirements)
         required_columns = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"]
-        if not all(col in request.data for col in required_columns):
+        if not all(col in passenger for col in required_columns):
             raise ValueError(f"Input data must contain the following columns: {required_columns}")
 
-        input_data = pd.DataFrame([request.data])
-
+        input_data = pd.DataFrame([passenger])
         input_data = drop_columns(input_data)
-        prediction = pipeline.predict(input_data)
+
+        print(type(input_data), input_data)
+
+        prediction = pipeline.predict(input_data.values)
     except Exception as e:
         return {"error": str(e)}
 
     return {"prediction": prediction[0]}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
